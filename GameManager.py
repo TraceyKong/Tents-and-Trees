49be57f11d0ac774bcd4af0a47e74c05c49345f8
marckatz <marc.f.katz@gmail.com>
2020-05-05 17:27:47 -0400

    Update GameManager.py

--- GameManager.py
+++ GameManager.py
@@ -1,52 +1,67 @@
 from PuzzleGenerator import PuzzleGenerator
+import time
 
 class GameManager:
-
-    def __init__(self, player, size=5):
-        self.size = 5
-        self.puzzleGenerator = PuzzleGenerator(size)
-        self.puzzle = self.puzzleGenerator.getPuzzle()
-        self.solution = self.puzzleGenerator.getSolution()
-        self.player = player
+    """ A class used to represent the game manager """
+    
+    def __init__(self, player, size=9):
+        self.size = size                                        # the size of the puzzle
+        self.puzzleGenerator = PuzzleGenerator(size)            # the puzzle generator
+        self.puzzle = self.puzzleGenerator.getPuzzle()          # the grid of the puzzle
+        self.solution = self.puzzleGenerator.getSolution()      # the grid of the solution
+        self.player = player                                    # the player
 
     def checkSolved(self):
+        """ Checks whether the puzzle grid is equal to the solution grid """
         return self.puzzle.map == self.solution.map
     
     def restart(self):
+        """ Restores the puzzle grid to the beginning """
         self.puzzle = self.puzzleGenerator.getPuzzle()
 
     def start(self):
+        """ Runs the puzzle """
+        turns = 0
+        restarts = 0
+        start = time.process_time()
         self.puzzle.displayGrid()
 
         while not self.checkSolved():
             move = self.player.getMove(self.puzzle)
-
+            turns = turns + 1
             if move != -1 and move != 0 and self.puzzle.checkWithinGrid(move[1]):
                 action = move[0]
                 pos = move[1]
 
+                # places tent on position
                 if action == "place":
                     print("Placed tent on ", pos[0],',',pos[1])
                     self.puzzle.setCellValue(pos, '#')
 
+                # erases tent on position
                 elif action == "erase":
                     print("Erased tent at ", pos[0],',',pos[1])
                     self.puzzle.setCellValue(pos, '-')
                 
                 self.puzzle.displayGrid()
 
+            # restarts the puzzle
             elif move == -1:
+                restarts = restarts + 1
                 print("Restart")
                 self.restart()
             
                 self.puzzle.displayGrid()
 
+            # prints the solution and ends the game
             elif move == 0:
                 print("Solution:")
                 self.solution.displayGrid()
                 break
 
-        print("Congrats! You've solved the puzzle.")
+        print("Congrats! You've solved the puzzle in " + str(turns) + " turns, "
+              + str(restarts) + " restarts, and in "+
+              str(time.process_time() - start) + " seconds.")
 
 
     
