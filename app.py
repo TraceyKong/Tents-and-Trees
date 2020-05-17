36aa4a01bc89a85b214b880e40f8613ddb3ea1c8
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2020-05-17 17:12:11 -0400

    improved select level, game board

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ def get_move():
     game = current_app.config['GAME']
     game.play(pos)
     grid = game.puzzle
-    status = game.solved
+    status = game.checkSolved()
 
     return jsonify(content=grid.getCellValue(pos), status=status, grid=grid.map)
