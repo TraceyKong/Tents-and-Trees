24bd7c31cee0971445fa0b47871d7683b863fe8b
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2020-05-19 16:50:29 -0400

    added all ai's

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, redirect, jsonify, current_ap
 from game.PuzzleGenerator import PuzzleGenerator
 from game.GameManager import GameManager
 from game.hai import HAI
-from game.ComputerAI import ComputerAI
+from game.BasicAI import ComputerAI as BasicAI
+from game.RandomAI import RandomAI
+from game.ComputerAI import ComputerAI as MRV
 import os
 from copy import deepcopy
 from time import process_time
@@ -51,6 +53,47 @@ def get_move():
 
     return jsonify(content=grid.getCellValue(pos), status=status)
 
+############################# Random AI Page ###############################
+@app.route('/random-home', methods=['GET','POST'])
+def random_home():
+    if request.method == 'POST':
+        level = request.form['random-level']
+        app.config['RANDOM_LEVEL'] = level
+        return redirect(url_for('random'))
+    else:
+        return render_template('random-home.html')
+
+@app.route('/random')
+def random():
+    app.config['RANDOM-AI'] = RandomAI()
+    app.config['GAME'] = GameManager(current_app.config['RANDOM-AI'], int(current_app.config['RANDOM_LEVEL']))
+
+    grid = current_app.config['GAME'].puzzle
+
+    grid.displayGrid()
+
+    colValues = grid.colValues
+    content = []
+    for r in range(grid.size):
+        row = [grid.rowValues[r]]
+        cells = [[grid.getCellValue((r, c)), (r,c)] for c in range(len(grid.map[r]))]
+        row.append(cells)
+        content.append(row)
+
+    size = grid.size
+
+    return render_template('random.html', colValues=colValues, content=content, size=size)
+
+@app.route('/solve_random')
+def solve_random():
+    t0 = process_time()
+    app.config['GAME'].start()
+    t0 = process_time() - t0
+
+    tents = app.config['GAME'].solution.getTents()
+
+    return jsonify(solved_content=tents, time=t0)
+
 ############################# Backtracking AI Page ###############################
 @app.route('/backtracking-home', methods=['GET','POST'])
 def backtracking_home():
@@ -63,7 +106,7 @@ def backtracking_home():
 
 @app.route('/backtracking')
 def backtracking():
-    app.config['BT-AI'] = ComputerAI()
+    app.config['BT-AI'] = BasicAI()
     app.config['GAME'] = GameManager(current_app.config['BT-AI'], int(current_app.config['BT_LEVEL']))
 
     grid = current_app.config['GAME'].puzzle
@@ -92,7 +135,7 @@ def solve_backtracking():
 
     return jsonify(solved_content=tents, time=t0)
 
-############################# Heuristic AI Page ###############################
+############################# Heuristic Backtracking AI Page ###############################
 @app.route('/heuristic-ai-home', methods=['GET','POST'])
 def heuristic_ai_home():
     if request.method == 'POST':
@@ -133,5 +176,46 @@ def solve_heuristic_ai():
     solved_grid = current_app.config['HAI']
 
     tents = solved_grid.getTents()
+
+    return jsonify(solved_content=tents, time=t0)
+
+############################# MRV AI Page ###############################
+@app.route('/mrv-ai-home', methods=['GET','POST'])
+def mrv_ai_home():
+    if request.method == 'POST':
+        level = request.form['mrv-level']
+        app.config['mrv_LEVEL'] = level
+        return redirect(url_for('mrv-ai'))
+    else:
+        return render_template('backtracking-home.html')
+
+@app.route('/mrv-ai')
+def mrv_ai():
+    app.config['MRV-AI'] = MRV()
+    app.config['GAME'] = GameManager(current_app.config['MRV-AI'], int(current_app.config['MRV_LEVEL']))
+
+    grid = current_app.config['GAME'].puzzle
+
+    grid.displayGrid()
+
+    colValues = grid.colValues
+    content = []
+    for r in range(grid.size):
+        row = [grid.rowValues[r]]
+        cells = [[grid.getCellValue((r, c)), (r,c)] for c in range(len(grid.map[r]))]
+        row.append(cells)
+        content.append(row)
+
+    size = grid.size
+
+    return render_template('mrv-ai.html', colValues=colValues, content=content, size=size)
+
+@app.route('/solve_mrv_ai')
+def solve_mrv_ai():
+    t0 = process_time()
+    app.config['GAME'].start()
+    t0 = process_time() - t0
+
+    tents = [tent[1] for tent in current_app.config['MRV-AI'].solvedTents]
 
     return jsonify(solved_content=tents, time=t0)
